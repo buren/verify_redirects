3c79e9f475a8544302ed0b9829443d6d3136f9f3
Jacob Burenstam <burenstam@gmail.com>
2018-08-16 15:18:33 +0200

    bundle gem

---
+++ lib/verify_redirects.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'verify_redirects/version'
+
+module VerifyRedirects
+  # Your code goes here...
+end
---
+++ spec/verify_redirects_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe VerifyRedirects do
+  it 'has a version number' do
+    expect(VerifyRedirects::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
